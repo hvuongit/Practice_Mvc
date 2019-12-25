10eb8e4ef81e24706d9d78cf1cf6813dd751d6ec
Vuong Vu <vuongit.dlu@gmail.com>
2019-12-25 17:30:52 +0700

    Demo Injecting Into Action Filters

--- Practice_Mvc/Controllers/IssueController.cs
+++ Practice_Mvc/Controllers/IssueController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Practice_Mvc.Data;
+using Practice_Mvc.Domain;
+using Practice_Mvc.Models.Issue;
 
 namespace Practice_Mvc.Controllers
 {
@@ -18,7 +21,72 @@ namespace Practice_Mvc.Controllers
         // GET: Issue
         public ActionResult IssueWidget()
         {
-            return Content("Here's where issue would go!");
+            var models = from i in _context.Issues
+                select new IssueSummaryViewModel
+                {
+                    IssueID = i.IssueID,
+                    Subject = i.Subject,
+                    CreatedAt = i.CreatedAt
+                };
+            return PartialView(models.ToArray());
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult New(NewIssueForm form)
+        {
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.Find(userId);
+            _context.Issues.Add(new Issue(user, form.Subject, form.Body));
+
+            _context.Logs.Add(new LogAction(user, "New", "Issue", "Created issue"));
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult View(int id)
+        {
+            var issue = _context.Issues.Find(id);
+            if (issue == null)
+            {
+                throw new ApplicationException("Issue not found!");
+            }
+
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.Find(userId);
+
+            _context.Logs.Add(new LogAction(user, "View", "Issue", "Viewed issue " + id));
+
+            return View(new IssueDetailsViewModel
+            {
+                IssueID = issue.IssueID,
+                Subject = issue.Subject,
+                CreatedAt = issue.CreatedAt,
+                Body = issue.Body
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var issue = _context.Issues.Find(id);
+            if (issue == null)
+            {
+                throw new ApplicationException("Issue not found!");
+            }
+
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.Find(userId);
+
+            _context.Logs.Add(new LogAction(user, "Delete", "Issue", "Deleted issue "+ id));
+
+            _context.Issues.Remove(issue);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
